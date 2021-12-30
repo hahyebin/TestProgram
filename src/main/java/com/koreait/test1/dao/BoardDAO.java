960ca9a6bdc742155f016427293c8aa6469b821c
hahyebin <hahyebin96@naver.com>
2021-12-30 15:24:08 +0900

    [2021-12-30] BoardDao class->interface 변경

--- src/main/java/com/koreait/test1/dao/BoardDAO.java
+++ src/main/java/com/koreait/test1/dao/BoardDAO.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Repository;
 import com.koreait.test1.dto.Board;
 
 @Repository
-public class BoardDAO {
+public interface BoardDAO {
 
-	public ArrayList<Board> selectBoardList() { return null; }
-	public int selectBoardCount() { return 0; }
+	public ArrayList<Board> selectBoardList();
+	public int selectBoardCount();
 //	public int insertBoard(String writer, String title, String content) { return 0; }
-	public int insertBoard(Board board) { return 0; }
-	public Board selectByIdx(int idx) { return null; }
+	public int insertBoard(Board board);
+	public Board selectByIdx(int idx);
 	//public int updateBoard(String title, String content, int idx) { return 0; }
-	public int updateBoard(Board board) { return 0; }
-	public int deleteBoard(int idx) { return 0; }
+	public int updateBoard(Board board);
+	public int deleteBoard(int idx);
 	
 }
